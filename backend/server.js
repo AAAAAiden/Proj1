d2e545a4e7ccaff9744cf0e36b815288b173d17c
benchengli09 <libc3650@gmail.com>
2025-03-30 12:59:32 -0700

    clean server.js

--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,11 @@ const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 const productRoutes = require('./routes/productRoutes');
 const errorHandler = require('./utils/errorHandler');
-const cors = require('cors');
 
 dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
